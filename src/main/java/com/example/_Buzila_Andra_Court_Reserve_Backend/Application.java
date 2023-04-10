083f60bfb719c2c49ed0f8f6fecccbd200cdd9a5
Andra Buzila <Andra.Buzila@student.utcluj.ro>
2023-04-10 15:40:41 +0300

    Create backend project

---
+++ src/main/java/com/example/_Buzila_Andra_Court_Reserve_Backend/Application.java
@@ -0,0 +1,13 @@
+package com.example._Buzila_Andra_Court_Reserve_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/_Buzila_Andra_Court_Reserve_Backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._Buzila_Andra_Court_Reserve_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
