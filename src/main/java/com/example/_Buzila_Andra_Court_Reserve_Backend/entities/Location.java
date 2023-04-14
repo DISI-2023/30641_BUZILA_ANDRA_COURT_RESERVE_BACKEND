2e9d752b1c8a157894deac9171579608203c4386
Orsan Tudor <tudor.orsan@yahoo.com>
2023-04-14 13:31:39 +0300

    #24-B Create Location Entity

---
+++ src/main/java/com/example/_Buzila_Andra_Court_Reserve_Backend/entities/Location.java
@@ -0,0 +1,86 @@
+package com.example._Buzila_Andra_Court_Reserve_Backend.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+public class Location implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    //:, unique = true
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "longitude", nullable = false)
+    private int longitude;
+
+    @Column(name = "latitude", nullable = false)
+    private int latitude;
+
+    @Column(name = "courtsImage", nullable = false)
+    private String courtsImage;
+
+    //Constructor with id:
+    public Location(UUID id, String address, int longitude, int latitude, String courtsImage) {
+        this.id = id;
+        this.address = address;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.courtsImage = courtsImage;
+    }
+
+    //Constructor without id:
+    public Location(String address, int longitude, int latitude, String courtsImage) {
+        this.id = id;
+        this.address = address;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.courtsImage = courtsImage;
+    }
+
+    //Empty constructor:
+    public Location() {
+    }
+
+    //Getters and Setters:
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+    public int getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+    public String getCourtsImage() {
+        return courtsImage;
+    }
+    public void setCourtsImage(String courtsImage) {
+        this.courtsImage = courtsImage;
+    }
+}
